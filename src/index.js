fe0813b0059ea5f694e10695b44a8db9069c0bde
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-10-07 10:51:29 -0300

    cfg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import morgan from 'morgan';
+import paymentRoutes from './routes/payment.routes.js';
+import {PORT} from './config.js';
 
 const app = express();
-const PORT = 3000;
+app.use(morgan('dev'));
+
+app.use(paymentRoutes)
+
 app.listen(PORT)
 console.log('Server on port', PORT);
